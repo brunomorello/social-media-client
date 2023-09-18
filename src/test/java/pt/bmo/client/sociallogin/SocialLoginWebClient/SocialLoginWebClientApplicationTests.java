6acbcc3ea19e3f6d491868d02be601d1344aa683
Bruno Moreno <brunomorello7@gmail.com>
2023-09-18 20:24:20 +0100

    Initialize repo

---
+++ src/test/java/pt/bmo/client/sociallogin/SocialLoginWebClient/SocialLoginWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.bmo.client.sociallogin.SocialLoginWebClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
